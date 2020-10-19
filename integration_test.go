a3f6a1d102ec1207de3c086d74118af3d7e5c18a
Konstantinos <kos.tsakalozos@canonical.com>
2020-10-19 13:35:24 +0300

    Rename

--- integration_test.go
+++ integration_test.go
@@ -7,8 +7,8 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/freeekanayaka/kvsql/server"
-	"github.com/freeekanayaka/kvsql/server/config"
+	"github.com/ktsakalozos/kvsql/server"
+	"github.com/ktsakalozos/kvsql/server/config"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"gopkg.in/yaml.v2"
